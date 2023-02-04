36d2cd9234094027fc7cf011d0b55eb54a7c0552
juanandrade <juanm.96.andrade@gmail.com>
2023-02-04 00:05:37 -0500

    se mejora con decorador apiview y recibe get y post para consultar y agregar etapas de proyecto

--- desarrollo_empresarial/proyecto_empresarial/api.py
+++ desarrollo_empresarial/proyecto_empresarial/api.py
@@ -1,11 +1,19 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from proyecto_empresarial.models import EtapaProyecto
 from proyecto_empresarial.serializers import EtapaProyectoSerializer
 
-class EtapaProyectoAPIView(APIView):
+@api_view(['GET','POST'])
+def etapa_proyecto_api_view(request):
 
-    def get(slef, request):
-        etapa = EtapaProyecto.objects.all()
-        etapa_serializer = EtapaProyectoSerializer(etapa, many = True)
-        return Response(etapa_serializer.data)
+    if request.method == 'GET':
+        etapas = EtapaProyecto.objects.all()
+        etapas_serializer = EtapaProyectoSerializer(etapas, many = True)
+        return Response(etapas_serializer.data)
+    elif request.method == 'POST':
+        etapa_serializer = EtapaProyectoSerializer(data = request.data)
+        if etapa_serializer.is_valid():
+            etapa_serializer.save()
+            return Response(etapa_serializer.data)
+        return Response(etapa_serializer.errors)
--- desarrollo_empresarial/proyecto_empresarial/urls.py
+++ desarrollo_empresarial/proyecto_empresarial/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from proyecto_empresarial.api import EtapaProyectoAPIView
+from proyecto_empresarial.api import etapa_proyecto_api_view
 
 urlpatterns=[
-    path('etapa_proyecto/', EtapaProyectoAPIView.as_view(), name="etapa_proyecto_api")
+    path('etapa_proyecto/', etapa_proyecto_api_view, name="etapa_proyecto_api")
 ]
