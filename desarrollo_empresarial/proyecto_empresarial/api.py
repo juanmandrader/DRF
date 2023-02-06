217f72cb5efdb3b3d7b8f4e5f446a8d2dc52c6f1
juanandrade <juanm.96.andrade@gmail.com>
2023-02-06 11:48:14 -0500

    se mejora el codigo, mas sintetico y se incluyen respuestas con manejo de status code HTTP y hizo decorador api_view

--- desarrollo_empresarial/proyecto_empresarial/api.py
+++ desarrollo_empresarial/proyecto_empresarial/api.py
@@ -1,5 +1,6 @@
+from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.views import APIView
+# from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from proyecto_empresarial.models import EtapaProyecto
 from proyecto_empresarial.serializers import EtapaProyectoSerializer
@@ -17,3 +18,33 @@ def etapa_proyecto_api_view(request):
             etapa_serializer.save()
             return Response(etapa_serializer.data)
         return Response(etapa_serializer.errors)
+
+
+@api_view(['GET','PUT','DELETE'])
+def etapa_proyecto_detail_api_view(request, pk=None):
+    # Queryset
+    etapa = EtapaProyecto.objects.filter(id=pk).first()
+
+    # validation
+    if etapa:
+
+        # retrieve    
+        if request.method == 'GET':
+            etapa_serializer = EtapaProyectoSerializer(etapa)
+            return Response(etapa_serializer.data, status = status.HTTP_200_OK)
+
+        # update
+        elif request.method == 'PUT':
+            etapa_serializer = EtapaProyectoSerializer(etapa, data = request.data)
+            if etapa_serializer.is_valid():
+                etapa_serializer.save()
+                return Response(etapa_serializer.data,  status = status.HTTP_200_OK)
+            return Response(etapa_serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+        # delete
+        elif request.method == 'DELETE':
+            etapa.delete()
+            return Response({"message" : "Etapa eliminada correctamente!"}, status = status.HTTP_200_OK)
+    
+    return Response({"message" : "No se ha encontrado una etapa con estos datos"},
+                        status = status.HTTP_400_BAD_REQUEST)
